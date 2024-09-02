e5270415969c297187387a2c257e8cef25b0333f
Codexup5 <olegshev455@gmail.com>
2024-09-02 15:25:36 +0500

    feature/user-table-0: Create app

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 3000,
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: true,
+    },
+});
